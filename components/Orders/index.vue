07d83ade0a74aaf00f7719c2b896f1eee9cf38df
gabrielgsv <gabriel.soares.vilela@gmail.com>
2023-12-07 16:46:33 -0300

    added get api to show orders

--- components/Orders/index.vue
+++ components/Orders/index.vue
@@ -1,9 +1,47 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+type OrderType = {
+  name: string;
+  payment_type: string;
+  phone: string;
+  total: number;
+  products_orders: {
+    price: number;
+    subtotal: number;
+    productId: number;
+    productName: string;
+    additional: {
+      name: string;
+      count: number;
+      value: number;
+    }[];
+  }[];
+  change: number;
+  address: string;
+  address_number: string;
+  neighborhood: string;
+  complement: string;
+  cep: string;
+  city: string;
+  state: string;
+};
+
+const userId = useCookie("userId");
+const orders = ref<OrderType[]>([]);
+
+onMounted(() => {
+  api()
+    .get(`/orders/${userId.value}`)
+    .then(({ data }) => {
+      orders.value = data as OrderType[];
+      debugger;
+    });
+});
+</script>
 
 <template>
   <div>
-    <h1>Orders</h1>
-    <p>Testando aoeuinfiouanfiuon</p>
+    <h1>Pedidos</h1>
+    {{ orders.map((order) => order.name) }}
   </div>
 </template>
 
