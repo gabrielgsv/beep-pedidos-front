a121627871b0895baafd6a300f2446f86c32b46c
gabrielgsv <gabriel.soares.vilela@gmail.com>
2024-03-06 21:19:57 -0300

    added payment button

---
+++ pages/Dashboard/Payment/index.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+function handleClickPay() {
+  api()
+    .post("/payment/create-checkout-session")
+    .then(({ data }) => {
+      window.location.href = data?.url;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+</script>
+
+<template>
+  <div>
+    <h1>Pagamento</h1>
+    <UButton @click="handleClickPay()">Pagar mensalidade de R$49,90</UButton>
+  </div>
+</template>
+
+<style lang="css" scoped></style>
